eb1482742924b685123deb7282d31c87d25e6e78
HENRI KOUAME <98271974+ikopro10@users.noreply.github.com>
2022-02-11 17:36:20 +0100

    Add files via upload

---
+++ Exo 2 Gomycode.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+nom_prenom ="Kouame henri"
+print(nom_prenom)
+
+
+# In[26]:
+
+
+som = 0
+n = 5
+nn = 55
+nnn = 555
+som = som + n + nn + nnn
+
+print(som)
+
+
+# In[31]:
+
+
+print(sum(i for i in range(2000, 3000) if i%7 and i!=5))
+
+
+# In[47]:
+
+
+
+
+
+# In[53]:
+
+
+prix = 500
+if prix >= 500 :
+    prix = (500*0.5)
+    print (prix)
+elif prix in rang(200, 500):
+     prix = rang * 0.3
+print(prix)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
